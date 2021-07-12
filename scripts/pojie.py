cfdb50ae3fef80bb2636dd5c5248e8c911e4bd76
WoL <45914900+WoLeo-Z@users.noreply.github.com>
2021-07-12 14:41:51 +0800

    Update pojie.py

--- scripts/pojie.py
+++ scripts/pojie.py
@@ -9,18 +9,16 @@ if pj_cookie:
 	url = "https://www.52pojie.cn/home.php?mod=task&do=apply&id=2"
 	headers = {"cookie": pj_cookie, "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4265.0 Safari/537.36 Edg/87.0.644.4"}
 	req = requests.get(url, headers=headers).text
-	patt = '''<div id="messagetext" class="alert_info"><p>([\s\S]*?)</p>'''
-	pattern = re.compile(patt)
-	list = re.findall(r'<div id="messagetext" class="alert_info">\n<p>([\s\S]*?)</p>', req, re.M)
-	tip = list[0]
+	text = re.findall(r'<div id="messagetext" class="alert_info">\n<p>([\s\S]*?)</p>', req, re.M)
+	tip = text[0]
 	if "您需要先登录才能继续本操作" in tip:
 		status = "未登陆 / 登陆失败"
-		msg = "cookie失效，请重新获取cookie"
+		message = "cookie失效，请重新获取cookie"
 	elif "本期您已申请过此任务" in tip:
 		status = "今日已签到"
-		msg = "重复签到"
+		message = "重复签到"
 	elif "恭喜" in tip:
 		status = "签到成功"
-		msg = "任务已完成"
-	msg = status + ' - ' + msg + '\n' + tip
+		message = "任务已完成"
+	msg = status + ' - ' + message + '\n' + tip
 	print(msg)
